b247558b7fd6fe838c076436f44b9ebfa8ff6bd7
mich4ld <mchldziuba@gmail.com>
2021-11-12 22:55:37 +0100

    refactor socks5 client implementation

--- lib/constants.ts
+++ lib/constants.ts
@@ -14,26 +14,3 @@ export enum MimeTypes {
 export const headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; rv:78.0) Gecko/20100101 Firefox/78.0',
 }
-
-export function getSocksError(status: number) {
-    switch(status) {
-        case 0x01:
-            return 'General failure';
-        case 0x02:
-            return 'Connection not allowed by ruleset';
-        case 0x03:
-            return 'Network unreachable';
-        case 0x04:
-            return 'Host unreachable';
-        case 0x05:
-            return 'Connection refused by destination host';
-        case 0x06:
-            return 'TTL expired';
-        case 0x07:
-            return 'Command not supported / protocol error';
-        case 0x08:
-            return 'Address type not supported';
-        default:
-            return 'Unknown SOCKS response status';
-    }
-}
--- lib/socks.ts
+++ lib/socks.ts
@@ -1,10 +1,9 @@
 import net, { Socket, isIP } from 'net';
-import { getSocksError } from './constants';
 import { SocksOptions } from './types';
 
-const socksVersion = 0x05;
-const authMethods = 0x01;
-const noPassMethod = 0x00;
+const socksVersion = 0x05; // SOCKS v5
+const authMethods = 0x01; // NUMBER OF SUPPORTED AUTH METHODS
+const noPassMethod = 0x00; // PREFERED AUTH METHOD BY DEFAULT (no password)
 
 export class Socks {
     readonly socket: Socket;
@@ -18,32 +17,39 @@ export class Socks {
         });
     }
 
-    connect(host: string, port: number) {
+    async connect(host: string, port: number) {
+        await this.auth();
+        return this.request(host, port);
+    }
+
+    auth() {
         const request = [socksVersion, authMethods, noPassMethod];
 
-        return new Promise<Socket>((resolve, reject) => {
+        return new Promise<boolean>((resolve, reject) => {
             this.socket.once('data', chunk => {
                 if (chunk.length !== 2) {
-                    throw new Error('Invalid SOCKS response size');
+                    const err = new Error('Invalid SOCKS response size');
+                    return reject(err);
                 }
     
                 if (chunk[0] !== socksVersion) {
-                    throw new Error('Invalid SOCKS version in response');
+                    const err = new Error('Invalid SOCKS version in response');
+                    return reject(err);
                 }
     
                 if (chunk[1] !== noPassMethod) {
-                    throw new Error('Unexpected SOCKS authentication method');
+                    const err = new Error('Unexpected SOCKS authentication method');
+                    return reject(err);
                 }
                 
-                this.request(host, port, resolve, reject);
+                resolve(true);
             });
 
             this.socket.write(Buffer.from(request));
-        })
-        
+        });
     }
 
-    private request(host: string, port: number, resolve: any, reject: any) {
+    request(host: string, port: number) {
         const cmd = 0x01; // TCP/IP stream connection;
         const reserved = 0x00; // reserved byte;
         const parsedHost = parseHost(host); // parsed host type, host length and host value
@@ -53,24 +59,29 @@ export class Socks {
         const buffer = Buffer.from(request);
         buffer.writeUInt16BE(port, buffer.length - 2);
 
-        this.socket.once('data', chunk => {
-            if (chunk[0] !== socksVersion) {
-                throw new Error('Invalid SOCKS version in response');
-            }
-
-            if (chunk[1] !== 0x00) {
-                const errorMessage = getSocksError(chunk[1]);
-                throw new Error(errorMessage);
-            }
-
-            if (chunk[2] !== reserved) {
-                throw new Error('Invalid SOCKS response shape');
-            }
-
-            resolve(this.socket);
-        });
-
-        this.socket.write(buffer);
+        return new Promise<Socket>((resolve, reject) => {
+            this.socket.once('data', chunk => {
+                if (chunk[0] !== socksVersion) {
+                    const err =  new Error('Invalid SOCKS version in response');
+                    return reject(err);
+                }
+    
+                if (chunk[1] !== 0x00) {
+                    const errorMessage = getSocksError(chunk[1]);
+                    const err = new Error(errorMessage);
+                    return reject(err);
+                }
+    
+                if (chunk[2] !== reserved) {
+                    const err = new Error('Invalid SOCKS response shape');
+                    return reject(err);
+                }
+    
+                resolve(this.socket);
+            });
+    
+            this.socket.write(buffer);
+        });   
     }
 }
 
@@ -86,4 +97,27 @@ function parseHost(host: string) {
     const request = [0x03, len, ...parsedHostname];
 
     return request;
+}
+
+function getSocksError(status: number) {
+    switch(status) {
+        case 0x01:
+            return 'General failure';
+        case 0x02:
+            return 'Connection not allowed by ruleset';
+        case 0x03:
+            return 'Network unreachable';
+        case 0x04:
+            return 'Host unreachable';
+        case 0x05:
+            return 'Connection refused by destination host';
+        case 0x06:
+            return 'TTL expired';
+        case 0x07:
+            return 'Command not supported / protocol error';
+        case 0x08:
+            return 'Address type not supported';
+        default:
+            return 'Unknown SOCKS response status';
+    }
 }
